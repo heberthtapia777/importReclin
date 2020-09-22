a279221e568c8c754295023d901b1a408be58084
n4ch1t0 <n4ch0.lopez@gmail.com>
2020-09-21 23:44:55 -0400

    prueba de git con vscode

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in </p>
+                    consequat. Duis aute irure dolor in house</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
